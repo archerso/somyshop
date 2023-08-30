c312fdda7b90611d06f08541425e3a3eee4337f8
archers <badarcher.so@gmail.com>
2023-08-30 15:49:55 +0200

    gestion produit sans le bouton

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProduitController extends AbstractController
 {
@@ -30,4 +34,29 @@ class ProduitController extends AbstractController
         return $this->render('produit/show.html.twig');
     }
 
+    #[Route('/produit/form', name: 'produit_form')]
+    public function form(Request $request, EntityManagerInterface $manager) 
+    //! la class Request contient les données véhiculées par les superglobales
+    {
+        $produit = new Produit;
+        $form = $this->createForm(ProduitType::class, $produit);
+        
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+        $manager->persist($produit);
+        $manager->flush();
+        }
+        return $this->render('produit/form.html.twig',[
+            'formProduit' => $form
+        ]);
+    }
+    // #[Route('/produit/form', name: 'produit_formshow')]
+    // return $this->render('nomducontrollerenminuscule/nomdufichierdeformulaire.html.twig', [
+    //     // on renvoie le formulaire à la vue
+    //      'form'=>$form->createView()
+     
+    //  ]);
 }
+
+
